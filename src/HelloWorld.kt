3c9940631878e4707874f1bcb54cd5da44abf781
mabenteng <mabenteng@baidu.com>
2020-05-25 15:28:25 +0800

    first commit

---
+++ src/HelloWorld.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var h = HelloWorld();
+    val i = 1;
+    h.print();
+}
+
+class HelloWorld {
+    fun print() {
+        print("hello")
+    }
+
+    override fun toString(): String {
+        return super.toString()
+    }
+}
